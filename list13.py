0e9f85daf50eafc3dd01dd9468d9a8305d49eb3c
RiditCodes <90456597+RiditCodes@users.noreply.github.com>
2024-03-21 19:01:30 +0530

    changes

--- list13.py
+++ list13.py
@@ -1,3 +1,17 @@
 fruits = ["Mango", "Guava", "Orange", "Guava"]
 
 print(fruits.index("Guava"))
+
+fruits.insert(2, "Papaya")
+
+print(fruits)
+
+print(fruits.count("Guava"))
+
+fruits.pop(3)
+
+print(fruits)
+
+fruits.pop()
+
+print(fruits)
