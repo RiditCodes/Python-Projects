0ab2b6dbe915073ad961616e47c09061dfec951f
RiditCodes <90456597+RiditCodes@users.noreply.github.com>
2024-07-20 11:50:24 +0530

    les go

--- tkinter_icons2.py
+++ tkinter_icons2.py
@@ -2,8 +2,9 @@ from tkinter import *
 
 root = Tk()
 root.title("Icon")
-root.iconbitmap(r"C:\Users\Ridit\OneDrive\Python Projects\spaceship.ico")
+root.iconbitmap("education_book.ico")
 
 button_quit = Button(root, text="Exit program", command=root.quit)
 button_quit.pack()
+
 root.mainloop()
